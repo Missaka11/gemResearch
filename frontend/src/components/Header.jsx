e939ab1d463f2eb7aa243922a67a4d90404ee10e
Missaka Abeyrathne <missakaabeyrathne@192.168.1.3>
2025-02-11 02:20:56 +0530

    Added header buttons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,12 +3,56 @@ import "../styles/Header.css";
 
 export const Header = () => {
   return (
-    <header>
-      <img
-        src="https://i.imgur.com/WHoCqFA.png"
-        className="header-Logo rounded-3"
-        alt="Logo image"
-      />
+    <header className="pt-1 pb-1">
+      <div className="header-content mx-auto d-flex">
+        <img
+          src="https://i.imgur.com/WHoCqFA.png"
+          className="header-Logo rounded-3"
+          alt="Logo image"
+        />
+        <nav class="navbar navbar-expand-lg ms-auto">
+          <div class="container-fluid">
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item px-4">
+                  <a class="nav-link" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item px-4">
+                  <a class="nav-link" aria-current="page" href="#">
+                    About Us
+                  </a>
+                </li>
+                <li class="nav-item px-4">
+                  <a class="nav-link" aria-current="page" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li class="nav-item px-4">
+                  <a class="nav-link" aria-current="page" href="#">
+                    Service
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <button type="button" className="btn header-signin my-auto ms-5">
+          Sign In
+        </button>
+      </div>
     </header>
   );
 };
