a4d0d86ddb79d0a3c8d50e85ed825bd4ec16376e
Missaka11 <missaka.amandeepa@gmail.com>
2025-03-09 22:22:59 +0530

    Added deleted footer

--- frontend/src/components/FooterComp.jsx
+++ frontend/src/components/FooterComp.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "../styles/FooterComp.css";
 
 export const FooterComp = () => {
   return (
     <div>
-      
+      <div className="footer-emailDiv">sample@gmail.com</div>
+      <div className="footer-socialDiv text-center">© Gemora</div>
     </div>
   );
 };
