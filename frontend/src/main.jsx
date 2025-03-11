de4ae765124b974adb8fcc3cd410b900552ff8f3
Banula-Vidmin <banula.business1022@gmail.com>
2025-03-11 23:10:04 +0530

    gem shape preview and result page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import ImageCaptureRS from "./pages/ImageCaptureRS.jsx";
+import GemShapeCapture from "./pages/GemShapeCapture.jsx";
+import LiveReviewGemShape from "./pages/LiveReviewGemShape.jsx";
+import GemShapePreview from "./pages/GemShapePreview.jsx";
+import GemShapeResult from "./pages/GemShapeResult.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,6 +16,10 @@ createRoot(document.getElementById("root")).render(
         <Route index element={<App />} />
         <Route path="/" element={<App />}/>
         <Route path="/ImageCaptureRS" element={<ImageCaptureRS />}/>
+        <Route path="/GemShapeCapture" element={<GemShapeCapture />}/>
+        <Route path="/GemShapeCapture/LiveReviewGemShape" element={<LiveReviewGemShape />}/>
+        <Route path="/GemShapePreview" element={<GemShapePreview />}/>
+        <Route path="/GemShapeResult" element={<GemShapeResult />}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
