2f41f52eaf2f499d53db6b66bbc84670944e04a6
Banula-Vidmin <banula.business1022@gmail.com>
2025-03-12 22:02:57 +0530

    add jewellery design navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -74,8 +74,7 @@ function App() {
             data-aos-delay="300"
           >
             <NavLink
-              // to={"./ImageCaptureRS"}
-
+              to={"./GemShapeCapture"}
               style={{ color: "#212529", textDecorationLine: "none" }}
             >
               <div className="team-member hover-effect">
